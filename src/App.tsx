cab219a0b1e8ec3a7da7af39fc940872226ad96d
SAM50x <149119430+Sam50x@users.noreply.github.com>
2025-10-27 20:34:25 +0300

    projects file

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import Projects from "./sections/Projects"
+
 function App() {
 
   return (
-    <main className="bg-background min-h-screen w-full xl:px-25 lg:px-16 md:px-8 px-3 transition-all duration-500">
-      
+    <main className="bg-background min-h-screen w-full xl:px-25 lg:px-16 md:px-8 px-3 transition-all duration-500 font-gugi">
+      <Projects />
     </main>
   )
 }
---
+++ src/sections/Projects.tsx
@@ -0,0 +1,9 @@
+const Projects = () => {
+    return (
+        <section className="w-full min-h-screen flex flex-col justify-start items-center">
+            <h4 className="mt-8 text-text font-gugi text-md">Projects</h4>
+        </section>
+    )
+}
+
+export default Projects
