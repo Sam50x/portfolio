1a04c48365914efd2291b0bd6d1e26a96e3d4c85
SAM50x <149119430+Sam50x@users.noreply.github.com>
2025-10-27 19:57:10 +0300

    app init

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <main className="bg-background min-h-screen w-full xl:px-25 lg:px-16 md:px-8 px-3 transition-all duration-500">
+      
+    </main>
+  )
+}
+
+export default App
