6730a7f75a0d0138df6041f50b5b2413abb73b57
SAM50x <149119430+Sam50x@users.noreply.github.com>
2025-10-31 17:16:13 +0200

    animation 🙌

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -1,44 +1,70 @@
 import clsx from 'clsx'
 import { useState } from 'react'
+import { motion, AnimatePresence } from 'framer-motion'
 
 import nextArrow from '../assets/icons/nextArrow.svg'
 import prevArrow from '../assets/icons/prevArrow.svg'
 
+type Position = 'none' | 'beforePrev' | 'prev' | 'current' | 'next' | 'afterNext'
+
 const Projects = () => {
     const [currentIndex, setCurrentIndex] = useState<number>(0)
+    const [isAnimating, setIsAnimating] = useState<boolean>(false)
 
     const handleGetNext = () => {
-        if (currentIndex === 5) return
+        if (currentIndex === 5 || isAnimating) return
+        setIsAnimating(true)
         setCurrentIndex(prev => prev + 1)
+        setTimeout(() => setIsAnimating(false), 500)
     }
 
     const handleGetPrev = () => {
-        if (currentIndex === 0) return
+        if (currentIndex === 0 || isAnimating) return
+        setIsAnimating(true)
         setCurrentIndex(prev => prev - 1)
+        setTimeout(() => setIsAnimating(false), 500)
+    }
+
+    const positions = {
+        none: 'w-0 h-0 hidden',
+        beforePrev: 'bg-cards-dark-inactive scale-50 -rotate-10 z-0 -translate-y-40 -translate-x-3000 md:scale-70 md:translate-y-0 lg:rotate-0 lg:scale-80 opacity-0 h-0 w-none',
+        prev: 'bg-cards-dark-inactive scale-50 -rotate-10 z-1 -translate-y-40 -translate-x-30 md:scale-70 md:translate-y-0 md:-translate-x-50 lg:rotate-0 lg:-translate-x-75 lg:scale-80 h-106 w-70',
+        current: 'bg-cards-dark scale-90 md:scale-100 lg:scale-110 hover:border-text hover:border z-2 h-106 w-70',
+        next: 'bg-cards-dark-inactive scale-50 rotate-10 z-1 -translate-y-40 translate-x-30 md:scale-70 md:translate-y-0 md:translate-x-50 lg:rotate-0 lg:translate-x-75 lg:scale-80 h-106 w-70',
+        afterNext: 'bg-cards-dark-inactive scale-50 rotate-10 z-0 -translate-y-40 translate-x-3000 md:scale-70 md:translate-y-0 lg:rotate-0 lg:scale-80 opacity-0 h-0 w-none',
     }
 
     return (
         <section className="w-full min-h-screen flex flex-col justify-start items-center">
             <h4 className="mt-8 text-text text-md">Projects</h4>
-            <div id="cards" className="w-full flex justify-center items-center flex-1">
-                {currentIndex > 0 &&
-                    <div id='prev' className={clsx("bg-cards-dark-inactive h-106 w-70 scale-50 rounded-xl transition-all duration-500 -rotate-10 absolute z-1 -translate-y-40 -translate-x-30 md:scale-70 md:translate-y-0 md:-translate-x-50 lg:rotate-0 lg:-translate-x-75 lg:scale-80"
-                    )}>
-                        <p className='text-text'>{currentIndex - 1}</p>
-                    </div>
-                }
-                {currentIndex >= 0 && currentIndex <= 5 &&
-                    <div id="current" className={clsx("bg-cards-dark h-106 w-70 scale-90 md:scale-100 lg:scale-110 rounded-xl hover:border-text hover:border transition-all duration-500 z-2"
-                    )}>
-                        <p className='text-text'>{currentIndex}</p>
-                    </div>
-                }
-                {currentIndex < 5 &&
-                    <div id='next' className={clsx("bg-cards-dark-inactive h-106 w-70 scale-50 rounded-xl transition-all duration-500 rotate-10 absolute z-1 -translate-y-40 translate-x-30 md:scale-70 md:translate-y-0 md:translate-x-50 lg:rotate-0 lg:translate-x-75 lg:scale-80"
-                    )}>
-                        <p className='text-text'>{currentIndex + 1}</p>
-                    </div>
-                }
+            <div id="cards" className="w-full flex justify-center items-center flex-1 overflow-hidden relative">
+                <AnimatePresence>
+                    {[0, 1, 2, 3, 4, 5].map((_, i) => {
+                        if (i < 0 || i > 5) return null;
+                        let pos: Position = 'none'
+
+                        if (i == currentIndex) pos = 'current'
+                        if (i == currentIndex - 1) pos = 'prev'
+                        if (i == currentIndex + 1) pos = 'next'
+                        if (i == currentIndex - 2) pos = 'beforePrev'
+                        if (i == currentIndex + 2) pos = 'afterNext'
+
+                        return (
+                            <motion.div
+                                key={i}
+                                layout
+                                transition={{ duration: 0.5, ease: 'easeInOut' }}
+                                className={clsx(
+                                    'absolute transition-all duration-500 rounded-xl flex overflow-hidden',
+                                    positions[pos]
+                                )}
+                            >
+                                <p className="text-text text-4xl font-bold">{i}</p>
+                            </motion.div>
+                        )
+                    })
+                    }
+                </AnimatePresence>
             </div>
             <div className='mb-8 flex flex-row justify-center items-center'>
                 <button
