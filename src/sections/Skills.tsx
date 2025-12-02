8a0e6df9d72db42b7e1fe53d06853743262c3c41
SAM50x <149119430+Sam50x@users.noreply.github.com>
2025-12-02 09:14:51 +0200

    deactivate on double click

--- src/sections/Skills.tsx
+++ src/sections/Skills.tsx
@@ -28,7 +28,9 @@ const Skills = () => {
     })
 
     return (
-        <section id='skills' className="w-full h-screen flex flex-col justify-start items-center pb-0.5">
+        <section id='skills' className="w-full h-screen flex flex-col justify-start items-center pb-0.5"
+            onDoubleClick={deactivateAnimation}
+        >
             <h4 className="mt-8 text-text text-md">Skills</h4>
             <h1 className="mt-12 text-text text-2xl text-center">What are you looking for?</h1>
             <div className="flex-1 w-full pt-12 flex flex-col justify-end items-center gap-0">
